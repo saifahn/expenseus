82be186b6dcc22b1804eeeb995f97a19187794cf
Sean Li <li.saifahn@gmail.com>
2021-07-04 11:45:12 +0900

    test: add test for another id

--- webservice.go
+++ webservice.go
@@ -3,8 +3,20 @@ package expenseus
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func WebService(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Expense 1")
+	expenseId := strings.TrimPrefix(r.URL.Path, "/expenses/")
+
+	if expenseId == "9281" {
+		fmt.Fprint(w, "Expense 9281")
+		return
+	}
+
+	if expenseId == "1" {
+		fmt.Fprint(w, "Expense 1")
+		return
+	}
+
 }
--- webservice_test.go
+++ webservice_test.go
@@ -20,4 +20,18 @@ func TestGetExpenses(t *testing.T) {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	})
+
+	t.Run("gets another expense by id", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "/expenses/9281", nil)
+		response := httptest.NewRecorder()
+
+		WebService(response, request)
+
+		got := response.Body.String()
+		want := "Expense 9281"
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
 }

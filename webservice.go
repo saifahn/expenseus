66d732a9bb353685c79d5b7a2cd321ab589c3ea9
Sean Li <li.saifahn@gmail.com>
2021-07-04 18:07:47 +0900

    feat: add create expense handler

--- webservice.go
+++ webservice.go
@@ -1,6 +1,7 @@
 package expenseus
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
@@ -9,11 +10,12 @@ import (
 type ExpenseStore interface {
 	GetExpenseNameById(id string) string
 	GetExpenseNamesByUser(user string) []string
+	RecordExpense(expense Expense)
 }
 
 type Expense struct {
-	name string
-	user string
+	Name string
+	User string
 }
 
 func NewWebService(store ExpenseStore) *WebService {
@@ -29,6 +31,7 @@ func (wb *WebService) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	router := http.NewServeMux()
 	router.HandleFunc("/expenses/user/", wb.expenseByUserHandler)
 	router.HandleFunc("/expenses/", wb.expenseByIdHandler)
+	router.HandleFunc("/expenses", wb.createExpenseHandler)
 
 	router.ServeHTTP(w, r)
 }
@@ -51,4 +54,17 @@ func (wb *WebService) expenseByUserHandler(rw http.ResponseWriter, r *http.Reque
 	expenses := wb.store.GetExpenseNamesByUser(user)
 
 	fmt.Fprint(rw, expenses)
+}
+
+func (wb *WebService) createExpenseHandler(rw http.ResponseWriter, r *http.Request) {
+	var e Expense
+	err := json.NewDecoder(r.Body).Decode(&e)
+
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	wb.store.RecordExpense(e)
+	rw.WriteHeader(http.StatusAccepted)
 }
--- webservice_test.go
+++ webservice_test.go
@@ -1,6 +1,8 @@
 package expenseus
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -8,13 +10,13 @@ import (
 )
 
 var testSeanExpense = Expense{
-	name: "Expense 1",
-	user: "sean",
+	Name: "Expense 1",
+	User: "sean",
 }
 
 var testTomomiExpense = Expense{
-	name: "Expense 9281",
-	user: "tomomi",
+	Name: "Expense 9281",
+	User: "tomomi",
 }
 
 func TestGetExpenseById(t *testing.T) {
@@ -33,7 +35,7 @@ func TestGetExpenseById(t *testing.T) {
 		webservice.ServeHTTP(response, request)
 
 		assertResponseStatus(t, response.Code, http.StatusOK)
-		assertResponseBody(t, response.Body.String(), testSeanExpense.name)
+		assertResponseBody(t, response.Body.String(), testSeanExpense.Name)
 	})
 
 	t.Run("gets another expense by id", func(t *testing.T) {
@@ -43,7 +45,7 @@ func TestGetExpenseById(t *testing.T) {
 		webservice.ServeHTTP(response, request)
 
 		assertResponseStatus(t, response.Code, http.StatusOK)
-		assertResponseBody(t, response.Body.String(), testTomomiExpense.name)
+		assertResponseBody(t, response.Body.String(), testTomomiExpense.Name)
 	})
 
 	t.Run("returns 404 on non-existent expense", func(t *testing.T) {
@@ -72,7 +74,7 @@ func TestGetExpenseByUser(t *testing.T) {
 		webservice.ServeHTTP(response, request)
 
 		assertResponseStatus(t, response.Code, http.StatusOK)
-		assertResponseBody(t, response.Body.String(), fmt.Sprintf("[%v]", testTomomiExpense.name))
+		assertResponseBody(t, response.Body.String(), fmt.Sprintf("[%v]", testTomomiExpense.Name))
 	})
 
 	t.Run("gets Sean's expenses", func(t *testing.T) {
@@ -82,12 +84,39 @@ func TestGetExpenseByUser(t *testing.T) {
 		webservice.ServeHTTP(response, request)
 
 		assertResponseStatus(t, response.Code, http.StatusOK)
-		assertResponseBody(t, response.Body.String(), fmt.Sprintf("[%v]", testSeanExpense.name))
+		assertResponseBody(t, response.Body.String(), fmt.Sprintf("[%v]", testSeanExpense.Name))
+	})
+}
+
+func TestCreateExpense(t *testing.T) {
+	store := StubExpenseStore{
+		map[string]Expense{},
+	}
+	webservice := NewWebService(&store)
+
+	t.Run("creates a new expense on POST", func(t *testing.T) {
+		request := newCreateExpenseRequest("tomomi", "Test Expense")
+		response := httptest.NewRecorder()
+
+		webservice.ServeHTTP(response, request)
+
+		assertResponseStatus(t, response.Code, http.StatusAccepted)
+
+		if len(store.expenses) != 1 {
+			t.Errorf("got %d expenses, want %d", len(store.expenses), 1)
+		}
 	})
 }
 
 func newGetExpenseByIdRequest(id string) *http.Request {
 	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/expenses/%s", id), nil)
+	return req
+}
+
+func newCreateExpenseRequest(user, name string) *http.Request {
+	values := map[string]string{"user": user, "name": name}
+	jsonValue, _ := json.Marshal(values)
+	req, _ := http.NewRequest(http.MethodPost, "/expenses", bytes.NewBuffer(jsonValue))
 	return req
 }
 
@@ -111,15 +140,20 @@ type StubExpenseStore struct {
 
 func (s *StubExpenseStore) GetExpenseNameById(id string) string {
 	expense := s.expenses[id]
-	return expense.name
+	return expense.Name
 }
 
 func (s *StubExpenseStore) GetExpenseNamesByUser(user string) []string {
 	var expenseNames []string
 	for _, e := range s.expenses {
-		if e.user == user {
-			expenseNames = append(expenseNames, e.name)
+		if e.User == user {
+			expenseNames = append(expenseNames, e.Name)
 		}
 	}
 	return expenseNames
+}
+
+func (s *StubExpenseStore) RecordExpense(e Expense) {
+	testId := fmt.Sprintf("tid-%v", e.Name)
+	s.expenses[testId] = e
 }

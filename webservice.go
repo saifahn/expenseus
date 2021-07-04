176e6d4249452296bb0684a963bdb205cb8a32b9
Sean Li <li.saifahn@gmail.com>
2021-07-04 16:49:22 +0900

    refactor: seaprate handler functions

--- webservice.go
+++ webservice.go
@@ -25,25 +25,30 @@ type WebService struct {
 }
 
 func (wb *WebService) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
 	router := http.NewServeMux()
-
-	router.Handle("/expenses/user/", http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		user := strings.TrimPrefix(r.URL.Path, "/expenses/user/")
-		expenses := wb.store.GetExpenseNamesByUser(user)
-		fmt.Fprint(rw, expenses)
-	}))
-
-	router.Handle("/expenses/", http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		expenseId := strings.TrimPrefix(r.URL.Path, "/expenses/")
-
-		expenseName := wb.store.GetExpenseNameById(expenseId)
-
-		if expenseName == "" {
-			rw.WriteHeader(http.StatusNotFound)
-		}
-
-		fmt.Fprint(rw, expenseName)
-	}))
+	router.HandleFunc("/expenses/user/", wb.expenseByUserHandler)
+	router.HandleFunc("/expenses/", wb.expenseByIdHandler)
 
 	router.ServeHTTP(w, r)
+}
+
+func (wb *WebService) expenseByIdHandler(rw http.ResponseWriter, r *http.Request) {
+	expenseId := strings.TrimPrefix(r.URL.Path, "/expenses/")
+
+	expenseName := wb.store.GetExpenseNameById(expenseId)
+
+	if expenseName == "" {
+		rw.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(rw, expenseName)
+}
+
+func (wb *WebService) expenseByUserHandler(rw http.ResponseWriter, r *http.Request) {
+	user := strings.TrimPrefix(r.URL.Path, "/expenses/user/")
+
+	expenses := wb.store.GetExpenseNamesByUser(user)
+
+	fmt.Fprint(rw, expenses)
 }

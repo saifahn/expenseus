5a0bf904cdc0399532b677a2e6520b53cb950b2c
sean <li.saifahn@gmail.com>
2021-10-23 01:31:52 +0900

    18: Host front end bundle in Go, connect front end and back end for development (#25)
    * feat: serve files from a web/dist folder
    
    * feat: use next export to build static site
    
    * refactor: use api/v1 route, use production env vars
    
    * feat: connect front and back end in development
    
    * style: formatting
    
    * docs: add basic README

---
+++ README.md
@@ -0,0 +1,62 @@
+# Expenseus
+
+## Running a development version
+
+### Front end
+
+```sh
+cd client
+
+# install dependencies
+yarn
+
+# run the dev server
+yarn dev
+```
+
+You can see the UI at http://127.0.0.1:3000
+
+### Back end
+
+1. Create an environment variable file with the variables found in `.env.example`
+   - e.g. if you are using `direnv`, you can copy the file to a `.envrc`
+2. Create a Google OAuth client on [Google Cloud Platform](https://console.cloud.google.com/)
+3. Fill in the details. The redirect URL by default should be `http://127.0.0.1:5000/api/v1/callback_google`
+4. After setting up the environment variables, build the webserver and run it.
+
+```sh
+# from the `server` directory
+go build ./cmd/webserver
+
+# run the application
+./webserver
+```
+
+## Serving the bundle from the Go application
+
+1. Build the front end
+
+   ```sh
+   cd client
+
+   # build a static version of the app, exported to `out` directory
+   yarn build
+
+   # create the `web` directory in the `server` directory if it doesn't exist
+   mkdir ../server/web
+
+   # move the built static app to the `web/dist` directory
+   mv out ../server/web/dist
+   ```
+
+2. Build the web server and run it
+
+   ```sh
+   # from the `server` directory
+   go build ./cmd/webserver
+
+   # run the application
+   ./webserver
+   ```
+
+Navigate to http://127.0.0.1:5000 to see the UI hosted in the Go application.
--- server/router.go
+++ server/router.go
@@ -14,38 +14,43 @@ func InitRouter(wb *WebService) *chi.Mux {
 	// Basic CORS
 	r.Use(cors.Handler(cors.Options{
 		// TODO: use environment variables to determine allowed origins
-		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedOrigins:   []string{"http://localhost:3000", "http://127.0.0.1:3000"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
+		AllowCredentials: true,
 		MaxAge:           300,
 	}))
 
-	r.Route("/expenses", func(r chi.Router) {
-		r.Use(wb.VerifyUser)
-		r.Get("/", wb.GetAllExpenses)
-		r.Post("/", wb.CreateExpense)
+	fs := http.FileServer(http.Dir("./web/dist"))
+	r.Handle("/*", fs)
 
-		r.Route("/user/{username}", func(r chi.Router) {
-			r.Use(UsernameCtx)
-			r.Get("/", wb.GetExpensesByUsername)
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Route("/expenses", func(r chi.Router) {
+			r.Use(wb.VerifyUser)
+			r.Get("/", wb.GetAllExpenses)
+			r.Post("/", wb.CreateExpense)
+
+			r.Route("/user/{username}", func(r chi.Router) {
+				r.Use(UsernameCtx)
+				r.Get("/", wb.GetExpensesByUsername)
+			})
+
+			r.Route("/{expenseID}", func(r chi.Router) {
+				r.Use(ExpenseIDCtx)
+				r.Get("/", wb.GetExpense)
+			})
 		})
 
-		r.Route("/{expenseID}", func(r chi.Router) {
-			r.Use(ExpenseIDCtx)
-			r.Get("/", wb.GetExpense)
+		r.Route("/users", func(r chi.Router) {
+			r.Use(wb.VerifyUser)
+			r.Get("/", wb.ListUsers)
+			r.Post("/", wb.CreateUser)
 		})
-	})
 
-	r.Route("/users", func(r chi.Router) {
-		r.Use(wb.VerifyUser)
-		r.Get("/", wb.ListUsers)
-		r.Post("/", wb.CreateUser)
+		r.Get("/login_google", wb.OauthLogin)
+		r.Get("/callback_google", wb.OauthCallback)
 	})
-
-	r.Get("/login_google", wb.OauthLogin)
-	r.Get("/callback_google", wb.OauthCallback)
 
 	return r
 }

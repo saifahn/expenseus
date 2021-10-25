10b4006d8a9dab679ac9004836cf04f3cf4cd3cc
Sean Li <li.saifahn@gmail.com>
2021-10-26 00:10:46 +0900

    feat: add basic sytling to nav

--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 export default function Layout({ children }) {
   return (
     <>
-      <nav>
-        <ul>
+      <nav className="container mx-auto mt-4">
+        <ul className="flex">
           <li>
             <Link href="/">
               <a>Home</a>
             </Link>
           </li>
-          <li>
+          <li className="ml-4">
             <Link href="/users">
               <a>Users</a>
             </Link>
           </li>
-          <li>
+          <li className="ml-4">
             <Link href="/expenses">
               <a>Expenses</a>
             </Link>

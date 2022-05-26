b18eb64691a5f0c4d0a8402ef2b724d46d0d1610
Sean Li <sean.saifahn.li@gmail.com>
2022-05-27 00:50:42 +0900

    feat: add tracker list and form
    new dependencies - react-hook-form, swc, validator

--- client/components/TrackersSubmitForm.tsx
+++ client/components/TrackersSubmitForm.tsx
@@ -1,13 +1,52 @@
+import { useUserContext } from 'context/user';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useSWRConfig } from 'swr';
+
+type Inputs = {
+  name: string;
+  users: string;
+};
+
+async function createTracker(data: Inputs) {
+  const payload = {
+    name: data.name,
+    users: data.users.split(','),
+  };
+  await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/trackers`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+    },
+    credentials: 'include',
+    body: JSON.stringify(payload),
+  });
+}
+
 export default function TrackersSubmitForm() {
+  const { user } = useUserContext();
+  const { mutate } = useSWRConfig();
+  const { register, handleSubmit } = useForm<Inputs>({
+    defaultValues: {
+      users: user.id,
+    },
+  });
+  const submitCallback: SubmitHandler<Inputs> = (data) => {
+    mutate(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/trackers/user/${user.id}`,
+      createTracker(data),
+    );
+  };
+
   return (
     <div className="mt-6">
-      <form className="border-4 p-6">
+      <form onSubmit={handleSubmit(submitCallback)} className="border-4 p-6">
         <h3 className="text-lg font-semibold">Create Tracker</h3>
         <div className="mt-4">
           <label className="block font-semibold" htmlFor="name">
             Name
           </label>
           <input
+            {...register('name')}
             className="appearance-none w-full border rounded leading-tight focus:outline-none focus:ring py-2 px-3 mt-2"
             type="text"
             id="name"
@@ -19,6 +58,7 @@ export default function TrackersSubmitForm() {
             Participants
           </label>
           <input
+            {...register('users')}
             className="appearance-none w-full border rounded leading-tight focus:outline-none focus:ring py-2 px-3 mt-2"
             type="text"
             id="participants"
--- client/pages/shared.tsx
+++ client/pages/shared.tsx
@@ -1,8 +1,24 @@
 import TrackersSubmitForm from 'components/TrackersSubmitForm';
+import { useUserContext } from 'context/user';
 import { useState } from 'react';
+import useSWR from 'swr';
+
+interface Tracker {
+  name: string;
+  users: string[];
+  id: string;
+}
+
+const fetcher = (url) =>
+  fetch(url, { credentials: 'include' }).then((res) => res.json());
 
 export default function Shared() {
+  const { user } = useUserContext();
   const [showing, setShowing] = useState<'home' | 'trackers'>('home');
+  const { data, error } = useSWR<Tracker[]>(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/trackers/user/${user.id}`,
+    fetcher,
+  );
 
   return (
     <>
@@ -28,6 +44,18 @@ export default function Shared() {
         {showing === 'trackers' && (
           <>
             <TrackersSubmitForm />
+            <div className="mt-4 p-4">
+              <h2 className="text-2xl">My Trackers</h2>
+              {data &&
+                data.map((tracker) => (
+                  <article className="p-2 border-2 mt-4" key={tracker.id}>
+                    <h3>{tracker.name}</h3>
+                    {tracker.users.map((user) => (
+                      <p key={user}>{user}</p>
+                    ))}
+                  </article>
+                ))}
+            </div>
           </>
         )}
       </section>

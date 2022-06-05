71b2f1994cacdf625ab577e539a0c364ddf1fd17
Sean Li <sean.saifahn.li@gmail.com>
2022-06-05 19:03:03 +0900

    refactor: convert GetUserByID test

--- server/internal/app/users_next_test.go
+++ server/internal/app/users_next_test.go
@@ -81,3 +81,55 @@ func TestListUsers(t *testing.T) {
 		})
 	}
 }
+
+func TestGetUserByID(t *testing.T) {
+	testUser := app.User{
+		Username: "test-user",
+	}
+
+	tests := map[string]struct {
+		userID   string
+		user     app.User
+		expectFn mock_app.MockAppFn
+		wantCode int
+	}{
+		"with no user returned from the store": {
+			userID: "not-found",
+			user:   app.User{},
+			expectFn: func(ma *mock_app.App) {
+				ma.MockStore.EXPECT().GetUser("not-found").Return(app.User{}, app.ErrDBItemNotFound).Times(1)
+			},
+			wantCode: http.StatusNotFound,
+		},
+		"with a user returned from the store": {
+			userID: testUser.Username,
+			user:   testUser,
+			expectFn: func(ma *mock_app.App) {
+				ma.MockStore.EXPECT().GetUser(testUser.Username).Return(testUser, nil).Times(1)
+			},
+			wantCode: http.StatusOK,
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert := assert.New(t)
+			a := mock_app.SetUp(t, tc.expectFn)
+
+			request := app.NewGetUserRequest(tc.userID)
+			response := httptest.NewRecorder()
+
+			handler := http.HandlerFunc(a.GetUser)
+			handler.ServeHTTP(response, request)
+
+			assert.Equal(tc.wantCode, response.Code)
+
+			if tc.wantCode == http.StatusOK {
+				var got app.User
+				err := json.NewDecoder(response.Body).Decode(&got)
+				assert.NoError(err)
+				assert.Equal(tc.user, got)
+			}
+		})
+	}
+}
--- server/internal/app/users_test.go
+++ server/internal/app/users_test.go
@@ -9,30 +9,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetUserByID(t *testing.T) {
-	t.Run("returns a users details if the user exists", func(t *testing.T) {
-		store := StubTransactionStore{users: []User{TestSeanUser}}
-		oauth := StubOauthConfig{}
-		a := New(&store, &oauth, &StubSessionManager{}, "", &StubImageStore{})
-
-		request := NewGetUserRequest(TestSeanUser.ID)
-		response := httptest.NewRecorder()
-
-		handler := http.HandlerFunc(a.GetUser)
-		handler.ServeHTTP(response, request)
-
-		var got User
-		err := json.NewDecoder(response.Body).Decode(&got)
-		if err != nil {
-			t.Fatalf("error parsing response from server %q into User struct, '%v'", response.Body, err)
-		}
-
-		assert.Equal(t, jsonContentType, response.Result().Header.Get("content-type"))
-		assert.Equal(t, http.StatusOK, response.Code)
-		assert.Equal(t, TestSeanUser, got)
-	})
-}
-
 func TestGetSelf(t *testing.T) {
 	t.Run("returns the user details from the stored session if the user exists", func(t *testing.T) {
 		store := StubTransactionStore{users: []User{TestSeanUser}}

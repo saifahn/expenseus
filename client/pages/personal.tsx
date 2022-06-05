f587882e53920bdff37efcea39d8d4d7069d8834
Sean Li <sean.saifahn.li@gmail.com>
2022-06-05 10:31:06 +0900

    feat: add delete to frontend

--- client/pages/personal.tsx
+++ client/pages/personal.tsx
@@ -1,6 +1,6 @@
 import TransactionSubmitForm from 'components/TransactionSubmitForm';
 import { useUserContext } from 'context/user';
-import useSWR from 'swr';
+import useSWR, { mutate } from 'swr';
 
 interface Transaction {
   name: string;
@@ -10,6 +10,16 @@ interface Transaction {
   imageUrl?: string;
 }
 
+async function deleteTransaction(txnId: string) {
+  await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/transactions/${txnId}`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+    },
+    credentials: 'include',
+  });
+}
+
 export default function Personal() {
   const { user } = useUserContext();
   const { data: transactions, error } = useSWR<Transaction[]>(() =>
@@ -17,6 +27,13 @@ export default function Personal() {
       ? `${process.env.NEXT_PUBLIC_API_BASE_URL}/transactions/user/${user.id}`
       : null,
   );
+
+  function handleDelete(txnId: string) {
+    mutate(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/transactions/user/${user.id}`,
+      deleteTransaction(txnId),
+    );
+  }
 
   return (
     <>
@@ -34,7 +51,15 @@ export default function Personal() {
         {transactions &&
           transactions.map((txn) => (
             <article className="p-2 border-2 mt-4" key={txn.id}>
-              <h3 className="text-lg">{txn.name}</h3>
+              <div className="flex justify-between">
+                <h3 className="text-lg">{txn.name}</h3>
+                <button
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold uppercase text-sm py-2 px-4 rounded focus:outline-none focus:ring"
+                  onClick={() => handleDelete(txn.id)}
+                >
+                  Delete
+                </button>
+              </div>
               <p>{txn.amount}</p>
               <p>{txn.userId}</p>
               <p>{txn.id}</p>

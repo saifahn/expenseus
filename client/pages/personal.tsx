1ed73732dc298ebfa8950419e4208b9d9448e50b
Sean Li <sean.saifahn.li@gmail.com>
2022-06-07 22:52:40 +0900

    refactor: flesh out ReadUpdateTxnForm

--- client/pages/personal.tsx
+++ client/pages/personal.tsx
@@ -1,6 +1,7 @@
 import TransactionSubmitForm from 'components/TransactionSubmitForm';
 import { useUserContext } from 'context/user';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import useSWR, { mutate } from 'swr';
 
 interface Transaction {
@@ -9,6 +10,7 @@ interface Transaction {
   userId: string;
   amount: number;
   imageUrl?: string;
+  date: string;
 }
 
 async function deleteTransaction(txnId: string) {
@@ -19,6 +21,105 @@ async function deleteTransaction(txnId: string) {
     },
     credentials: 'include',
   });
+}
+
+interface ReadUpdateTxnFormProps {
+  txn: Transaction;
+  onApply: () => void;
+  onCancel: () => void;
+}
+
+function ReadUpdateTxnForm({ txn, onApply, onCancel }: ReadUpdateTxnFormProps) {
+  const { register, formState } = useForm({
+    shouldUseNativeValidation: true,
+    defaultValues: {
+      transactionName: txn.name,
+      amount: txn.amount,
+      date: new Date(txn.date).toISOString().split('T')[0],
+      image: null,
+    },
+  });
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()} className="border-4 p-6 mt-4">
+      <h3 className="text-lg font-semibold">Update Transaction</h3>
+      <div className="mt-4">
+        <label className="block font-semibold" htmlFor="name">
+          Name
+        </label>
+        <input
+          {...register('transactionName', {
+            required: 'Please input a transaction name',
+          })}
+          className="appearance-none w-full border rounded leading-tight focus:outline-none focus:ring py-2 px-3 mt-2"
+          type="text"
+          id="transactionName"
+        />
+      </div>
+      <div className="mt-4">
+        <label className="block font-semibold" htmlFor="amount">
+          Amount
+        </label>
+        <input
+          {...register('amount', { required: 'Please input an amount' })}
+          className="appearance-none w-full border rounded leading-tight focus:outline-none focus:ring py-2 px-3 mt-2"
+          type="text"
+          inputMode="numeric"
+          pattern="[0-9]*"
+          id="amount"
+        />
+      </div>
+      <div className="mt-4">
+        <label className="block font-semibold" htmlFor="date">
+          Date
+        </label>
+        <input
+          {...register('date', { required: 'Please input a date' })}
+          className="appearance-none w-full border rounded leading-tight focus:outline-none focus:ring py-2 px-3 mt-2"
+          type="date"
+          id="date"
+        />
+      </div>
+      <div className="mt-4">
+        <label className="block font-semibold" htmlFor="addPicture">
+          Add a picture?
+        </label>
+        <input
+          {...register('image')}
+          id="addPicture"
+          type="file"
+          role="button"
+          aria-label="Add picture"
+          accept="image/*"
+        />
+      </div>
+      <div className="mt-4 flex justify-end">
+        {formState.isDirty ? (
+          <>
+            <button
+              className="hover:bg-slate-200 font-bold uppercase text-sm py-2 px-4 rounded focus:outline-none focus:ring"
+              onClick={() => onCancel()}
+            >
+              Cancel
+            </button>
+            <button
+              className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold uppercase text-sm py-2 px-4 rounded focus:outline-none focus:ring"
+              onClick={() => onApply()}
+            >
+              Apply
+            </button>
+          </>
+        ) : (
+          <button
+            className="hover:bg-slate-200 font-bold uppercase text-sm py-2 px-4 rounded focus:outline-none focus:ring"
+            onClick={() => onCancel()}
+          >
+            Close
+          </button>
+        )}
+      </div>
+    </form>
+  );
 }
 
 export default function Personal() {
@@ -41,21 +142,11 @@ export default function Personal() {
     <>
       <h1 className="text-4xl">Personal</h1>
       {selectedTxn ? (
-        <div className="mt-4 p-4 border-4">
-          <p>Selected transaction: {selectedTxn.name}</p>
-          <button
-            className="bg-indigo-500 hover:bg-indigo-700 text-white font-bold uppercase text-sm py-2 px-4 rounded focus:outline-none focus:ring"
-            onClick={() => setSelectedTxn(null)}
-          >
-            Apply
-          </button>
-          <button
-            className="hover:bg-slate-200 font-bold uppercase text-sm py-2 px-4 rounded focus:outline-none focus:ring"
-            onClick={() => setSelectedTxn(null)}
-          >
-            Cancel
-          </button>
-        </div>
+        <ReadUpdateTxnForm
+          txn={selectedTxn}
+          onApply={() => setSelectedTxn(null)}
+          onCancel={() => setSelectedTxn(null)}
+        />
       ) : (
         <>
           <div className="mt-4">
@@ -89,6 +180,7 @@ export default function Personal() {
                   <p>{txn.amount}</p>
                   <p>{txn.userId}</p>
                   <p>{txn.id}</p>
+                  <p>{new Date(txn.date).toDateString()}</p>
                 </article>
               ))}
           </div>

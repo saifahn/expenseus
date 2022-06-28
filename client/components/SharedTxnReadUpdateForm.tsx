fb26b6d1948e2c0172c52d48799ae30308106ea4
Sean Li <sean.saifahn.li@gmail.com>
2022-06-28 11:40:47 +0800

    feat: add details and location to sharedTxn

--- client/components/SharedTxnReadUpdateForm.tsx
+++ client/components/SharedTxnReadUpdateForm.tsx
@@ -1,35 +1,22 @@
-import { CategoryKey } from 'data/categories';
 import { Tracker } from 'pages/shared/trackers';
 import { SharedTxn } from 'pages/shared/trackers/[trackerId]';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useSWRConfig } from 'swr';
-import SharedTxnFormBase from './SharedTxnFormBase';
+import SharedTxnFormBase, {
+  createSharedTxnFormData,
+  SharedTxnFormInputs,
+} from './SharedTxnFormBase';
 
-type Inputs = {
-  id: string;
-  shop: string;
-  amount: number;
-  date: string;
-  settled?: boolean;
-  participants: string;
-  payer: string;
-  category: CategoryKey;
-};
-
-async function updateSharedTxn(data: Inputs, tracker: Tracker) {
-  const formData = new FormData();
+async function updateSharedTxn(
+  data: SharedTxnFormInputs,
+  tracker: Tracker,
+  txnID: string,
+) {
+  const formData = createSharedTxnFormData(data);
   formData.append('participants', tracker.users.join(','));
-  formData.append('shop', data.shop);
-  formData.append('amount', data.amount.toString());
-  if (!data.settled) formData.append('unsettled', 'true');
-  formData.append('category', data.category);
-  formData.append('payer', data.payer);
-
-  const unixDate = new Date(data.date).getTime();
-  formData.append('date', unixDate.toString());
 
   await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/trackers/${tracker.id}/transactions/${data.id}`,
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/trackers/${tracker.id}/transactions/${txnID}`,
     {
       method: 'PUT',
       headers: {
@@ -55,55 +42,31 @@ export default function SharedTxnReadUpdateForm({
   onCancel,
 }: Props) {
   const { mutate } = useSWRConfig();
-  const { register, handleSubmit, formState } = useForm<Inputs>({
+  const { register, handleSubmit, formState } = useForm<SharedTxnFormInputs>({
     shouldUseNativeValidation: true,
     defaultValues: {
-      shop: txn.shop,
+      location: txn.location,
       amount: txn.amount,
       date: new Date(txn.date).toISOString().split('T')[0],
       settled: !txn.unsettled,
       category: txn.category,
+      details: txn.details,
     },
   });
 
-  const submitCallback: SubmitHandler<Inputs> = (data) => {
-    data.id = txn.id;
+  const submitCallback: SubmitHandler<SharedTxnFormInputs> = (data) => {
     mutate(
       `${process.env.NEXT_PUBLIC_API_BASE_URL}/trackers/${tracker.id}/transactions`,
-      updateSharedTxn(data, tracker),
+      updateSharedTxn(data, tracker, txn.id),
     );
     onApply();
   };
 
-  const shopInputProps = register('shop', {
-    required: 'Please input a shop name',
-  });
-  const amountInputProps = register('amount', {
-    min: {
-      value: 1,
-      message: 'Please input a positive amount',
-    },
-    required: 'Please input an amount',
-  });
-  const dateInputProps = register('date', {
-    required: 'Please input a date',
-  });
-  const payerInputProps = register('payer', {
-    required: 'Please select a payer',
-  });
-  const settledInputProps = register('settled');
-  const categoryInputProps = register('category');
-
   return (
     <SharedTxnFormBase
       title="Update Shared Transaction"
-      shopInputProps={shopInputProps}
-      amountInputProps={amountInputProps}
-      dateInputProps={dateInputProps}
-      payerInputProps={payerInputProps}
-      settledInputProps={settledInputProps}
-      categoryInputProps={categoryInputProps}
       tracker={tracker}
+      register={register}
       onSubmit={handleSubmit(submitCallback)}
     >
       <div className="mt-4 flex justify-end">

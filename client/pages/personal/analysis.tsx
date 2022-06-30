19dd0938b703206bc7ed34fcf544cee4ed146ff3
Sean Li <sean.saifahn.li@gmail.com>
2022-06-30 12:35:36 +0800

    feat: add fetching transactions for analysis

--- client/pages/personal/analysis.tsx
+++ client/pages/personal/analysis.tsx
@@ -1,9 +1,90 @@
 import PersonalLayout from 'components/LayoutPersonal';
+import { fetcher } from 'config/fetcher';
+import { useUserContext } from 'context/user';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import useSWR, { useSWRConfig } from 'swr';
+import { Temporal } from 'temporal-polyfill';
+import { Transaction } from 'types/Transaction';
+import { plainDateStringToEpochSec } from 'utils/temporal';
+
+type Inputs = {
+  from: string;
+  to: string;
+};
 
 export default function PersonalAnalysis() {
+  const { user } = useUserContext();
+  const { mutate } = useSWRConfig();
+  const { register, handleSubmit, getValues } = useForm<Inputs>({
+    shouldUseNativeValidation: true,
+    defaultValues: {
+      from: Temporal.Now.plainDateISO().subtract({ months: 3 }).toString(),
+      to: Temporal.Now.plainDateISO().toString(),
+    },
+  });
+
+  const { data: txns, error } = useSWR<Transaction[]>(
+    'personal.analysis',
+    () => {
+      if (!user) return null;
+      const { from, to } = getValues();
+      const fromEpochSec = plainDateStringToEpochSec(from);
+      const toEpochSec = plainDateStringToEpochSec(to);
+      return fetcher(
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/transactions/user/${user.id}/range?from=${fromEpochSec}&to=${toEpochSec}`,
+      );
+    },
+  );
+
+  const submitCallback: SubmitHandler<Inputs> = (data) => {
+    mutate('personal.analysis');
+  };
+
   return (
     <>
-      <PersonalLayout>The Personal Analysis page</PersonalLayout>
+      <PersonalLayout>
+        <form className="border-4 p-6" onSubmit={handleSubmit(submitCallback)}>
+          <div className="mt-4">
+            <label className="block font-semibold" htmlFor="dateFrom">
+              From
+            </label>
+            <input
+              {...register('from', { required: 'Please input a date' })}
+              className="mt-2 w-full appearance-none rounded border py-2 px-3 leading-tight focus:outline-none focus:ring"
+              type="date"
+              id="dateFrom"
+            />
+          </div>
+          <div className="mt-4">
+            <label className="block font-semibold" htmlFor="dateTo">
+              To
+            </label>
+            <input
+              {...register('to', { required: 'Please input a date' })}
+              className="mt-2 w-full appearance-none rounded border py-2 px-3 leading-tight focus:outline-none focus:ring"
+              type="date"
+              id="dateTo"
+            />
+          </div>
+          <div className="mt-4 flex justify-end">
+            <button className="rounded bg-indigo-500 py-2 px-4 text-sm font-bold uppercase text-white hover:bg-indigo-700 focus:outline-none focus:ring">
+              Get details
+            </button>
+          </div>
+        </form>
+        <div className="mt-6">
+          {error && <div>Failed to load details</div>}
+          {txns === null && <div>Loading</div>}
+          {txns?.length === 0 && (
+            <div>No transactions for that time period</div>
+          )}
+          {txns?.length > 0 && (
+            <div>
+              You have {txns.length} transaction(s) from that time period
+            </div>
+          )}
+        </div>
+      </PersonalLayout>
     </>
   );
 }

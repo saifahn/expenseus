ce94c609e6813655fc0b57828b060f32a69dd874
Sean Li <sean.saifahn.li@gmail.com>
2022-09-24 15:07:28 +0900

    refactor: restructure personal page

--- client/components/LayoutPersonal.tsx
+++ client/components/LayoutPersonal.tsx
@@ -5,17 +5,17 @@ export default function PersonalLayout({ children }) {
     <>
       <nav className="mt-4">
         <ul className="flex">
-          <li className="flex">
-            <Link href="/personal/create">
-              <a className="rounded-md bg-violet-100 p-2 lowercase hover:bg-violet-200 active:bg-violet-300">
-                Create +
+          <li className="mr-4 flex flex-1">
+            <Link href="/personal/analysis">
+              <a className="w-full rounded-lg bg-emerald-50 py-3 px-4 font-medium lowercase text-black hover:bg-emerald-100 active:bg-emerald-200">
+                🔎 Analyze
               </a>
             </Link>
           </li>
-          <li className="flex">
-            <Link href="/personal/analysis">
-              <a className="ml-4 rounded-md p-2 lowercase hover:bg-slate-200 active:bg-slate-300">
-                Analyze
+          <li className="flex flex-1">
+            <Link href="/personal/create">
+              <a className="w-full rounded-lg bg-violet-50 py-3 px-4 font-medium lowercase text-black hover:bg-violet-100 active:bg-violet-200">
+                ➕ Create new
               </a>
             </Link>
           </li>

f860760b88547cd6cf81e552becf29bfd7b7f946
Sean Li <sean.saifahn.li@gmail.com>
2023-02-01 22:40:49 +0900

    feat: add 400 for invalid payload

--- client/pages/api/v1/transactions.test.ts
+++ client/pages/api/v1/transactions.test.ts
@@ -12,8 +12,21 @@ describe('/api/v1/transactions POST endpoint', () => {
 
   it('should return a 405 error if the method is not POST', async () => {
     const { req, res } = mockReqRes('GET');
+
     await createTxnHandler(req, res);
 
     expect(res.statusCode).toBe(405);
+  });
+
+  it('should return a 400 error if the payload is invalid', async () => {
+    const { req, res } = mockReqRes();
+
+    req._setBody({
+      invalid: 'property',
+      another: 'wrong-one',
+    });
+    await createTxnHandler(req, res);
+
+    expect(res.statusCode).toBe(400);
   });
 });
--- client/pages/api/v1/transactions.ts
+++ client/pages/api/v1/transactions.ts
@@ -1,4 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { z, ZodError } from 'zod';
+
+const createTxnPayloadSchema = z.object({
+  userId: z.string(),
+  location: z.string(),
+  amount: z.number(),
+  date: z.number(),
+  category: z.string(),
+  details: z.string(),
+});
 
 export default async function createTxnHandler(
   req: NextApiRequest,
@@ -6,5 +16,15 @@ export default async function createTxnHandler(
 ) {
   if (req.method !== 'POST') {
     res.status(405).json({ error: 'invalid method' });
+    return;
+  }
+
+  try {
+    createTxnPayloadSchema.parse(req.body);
+  } catch (err) {
+    if (err instanceof ZodError) {
+      res.status(400).json({ error: 'invalid payload' });
+      return;
+    }
   }
 }

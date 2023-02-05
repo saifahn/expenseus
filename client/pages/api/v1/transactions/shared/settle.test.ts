3057b4e699bd0a0dfb748557d909a65c1c0cf207
Sean Li <sean.saifahn.li@gmail.com>
2023-02-05 14:39:37 +0900

    feat: return 400 on bad input

--- client/pages/api/v1/transactions/shared/settle.test.ts
+++ client/pages/api/v1/transactions/shared/settle.test.ts
@@ -8,4 +8,12 @@ describe('settleTxnsHandler', () => {
 
     expect(res.statusCode).toBe(405);
   });
+
+  test('returns a 400 if the input is invalid', async () => {
+    const { req, res } = mockReqRes('POST');
+    req._setBody({ something: 'invalid' });
+    await settleTxnsHandler(req, res);
+
+    expect(res.statusCode).toBe(400);
+  });
 });
--- client/pages/api/v1/transactions/shared/settle.ts
+++ client/pages/api/v1/transactions/shared/settle.ts
@@ -1,4 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { withTryCatch } from 'utils/withTryCatch';
+import { z, ZodError } from 'zod';
+
+const payloadSchema = z.array(
+  z.object({
+    id: z.string(),
+    trackerId: z.string(),
+    participants: z.array(z.string()),
+  }),
+);
 
 export default async function settleTxnsHandler(
   req: NextApiRequest,
@@ -6,5 +16,10 @@ export default async function settleTxnsHandler(
 ) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'invalid method' });
+  }
+
+  const [parsed, err] = withTryCatch(() => payloadSchema.parse(req.body));
+  if (err instanceof ZodError) {
+    return res.status(400).json({ error: 'invalid input' });
   }
 }

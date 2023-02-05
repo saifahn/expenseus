60c8dee0dc43e26b331a06c08ecda6875bd64656
Sean Li <sean.saifahn.li@gmail.com>
2023-02-05 13:01:05 +0900

    wip: add zod parsing of query string

--- client/pages/api/v1/transactions/user/[userId]/range.test.ts
+++ client/pages/api/v1/transactions/user/[userId]/range.test.ts
@@ -8,4 +8,25 @@ describe('getTxnsByUserIdBetweenDates handler', () => {
 
     expect(res.statusCode).toBe(405);
   });
+
+  test('it returns a 400 if there is no from or to query or they cannot be parsed to numbers', async () => {
+    var { req, res } = mockReqRes('GET');
+    await getTxnsByUserIdBetweenDatesHandler(req, res);
+    expect(res.statusCode).toBe(400);
+
+    var { req, res } = mockReqRes('GET');
+    req.query = {
+      from: 'not-a-number',
+      to: '12345678',
+    };
+    await getTxnsByUserIdBetweenDatesHandler(req, res);
+    expect(res.statusCode).toBe(400);
+
+    var { req, res } = mockReqRes('GET');
+    req.query = {
+      from: '12345678',
+    };
+    await getTxnsByUserIdBetweenDatesHandler(req, res);
+    expect(res.statusCode).toBe(400);
+  });
 });
--- client/pages/api/v1/transactions/user/[userId]/range.ts
+++ client/pages/api/v1/transactions/user/[userId]/range.ts
@@ -1,4 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { withTryCatch } from 'utils/withTryCatch';
+import { z, ZodError } from 'zod';
+
+const queryStringSchema = z.object({
+  from: z.coerce.number(),
+  to: z.coerce.number(),
+});
 
 export default async function getTxnsByUserIdBetweenDatesHandler(
   req: NextApiRequest,
@@ -6,5 +13,10 @@ export default async function getTxnsByUserIdBetweenDatesHandler(
 ) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'invalid method' });
+  }
+
+  const [parsed, err] = withTryCatch(() => queryStringSchema.parse(req.query));
+  if (err instanceof ZodError) {
+    return res.status(400).json({ error: 'invalid query' });
   }
 }

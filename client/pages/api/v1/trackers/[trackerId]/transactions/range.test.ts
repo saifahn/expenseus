4385550ab0d58ec5b71ef38741ff129a5d188c2f
Sean Li <sean.saifahn.li@gmail.com>
2023-02-08 14:01:09 +0900

    wip: set up 400 and 401

--- client/pages/api/v1/trackers/[trackerId]/transactions/range.test.ts
+++ client/pages/api/v1/trackers/[trackerId]/transactions/range.test.ts
@@ -1,5 +1,8 @@
+import { getServerSession } from 'next-auth';
 import { mockReqRes } from 'tests/api/common';
 import getTxnsByTrackerBetweenDatesHandler from './range';
+
+const sessionMock = jest.mocked(getServerSession);
 
 describe('getTxnsByTrackerBetweenDatesHandler', () => {
   test('it returns a 405 for non-GET requests', async () => {
@@ -7,5 +10,38 @@ describe('getTxnsByTrackerBetweenDatesHandler', () => {
     await getTxnsByTrackerBetweenDatesHandler(req, res);
 
     expect(res.statusCode).toBe(405);
+  });
+
+  test('it returns a 400 if there is no from or to query or they cannot be parsed to numbers', async () => {
+    var { req, res } = mockReqRes('GET');
+    await getTxnsByTrackerBetweenDatesHandler(req, res);
+    expect(res.statusCode).toBe(400);
+
+    var { req, res } = mockReqRes('GET');
+    req.query = {
+      from: 'not-a-number',
+      to: '12345678',
+    };
+    await getTxnsByTrackerBetweenDatesHandler(req, res);
+    expect(res.statusCode).toBe(400);
+
+    var { req, res } = mockReqRes('GET');
+    req.query = {
+      from: '12345678',
+    };
+    await getTxnsByTrackerBetweenDatesHandler(req, res);
+    expect(res.statusCode).toBe(400);
+  });
+
+  test('it returns a 401 if there is no valid session', async () => {
+    const { req, res } = mockReqRes('GET');
+    req.query = {
+      from: '12345678',
+      to: '23456789',
+    };
+    sessionMock.mockResolvedValueOnce(null);
+    await getTxnsByTrackerBetweenDatesHandler(req, res);
+
+    expect(res.statusCode).toBe(401);
   });
 });
--- client/pages/api/v1/trackers/[trackerId]/transactions/range.ts
+++ client/pages/api/v1/trackers/[trackerId]/transactions/range.ts
@@ -1,4 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
+import { withTryCatch } from 'utils/withTryCatch';
+import { z, ZodError } from 'zod';
+
+const queryStringSchema = z.object({
+  from: z.coerce.number(),
+  to: z.coerce.number(),
+});
 
 export default async function getTxnsByTrackerBetweenDatesHandler(
   req: NextApiRequest,
@@ -6,5 +14,15 @@ export default async function getTxnsByTrackerBetweenDatesHandler(
 ) {
   if (req.method !== 'GET') {
     return res.status(405).send({ error: 'invalid method' });
+  }
+
+  var [parsed, err] = withTryCatch(() => queryStringSchema.parse(req.query));
+  if (err instanceof ZodError) {
+    return res.status(400).json({ error: 'invalid query' });
+  }
+
+  const session = await getServerSession();
+  if (!session) {
+    return res.status(401).json({ error: 'no valid session found' });
   }
 }

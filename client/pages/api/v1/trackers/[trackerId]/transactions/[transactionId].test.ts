8a53a8d693b766abcb0c2239cc467448afbda650
Sean Li <sean.saifahn.li@gmail.com>
2023-02-08 22:27:20 +0900

    wip: 400

--- client/pages/api/v1/trackers/[trackerId]/transactions/[transactionId].test.ts
+++ client/pages/api/v1/trackers/[trackerId]/transactions/[transactionId].test.ts
@@ -1,5 +1,8 @@
+import { getServerSession } from 'next-auth';
 import { mockReqRes } from 'tests/api/common';
 import bySharedTxnIdHandler from './[transactionId]';
+
+const sessionMock = jest.mocked(getServerSession);
 
 describe('bySharedTxnIdHandler', () => {
   test('it returns a 405 if the method is not PUT or DELETE', async () => {
@@ -14,5 +17,19 @@ describe('bySharedTxnIdHandler', () => {
     await bySharedTxnIdHandler(req, res);
 
     expect(res.statusCode).toBe(401);
+  });
+
+  describe('PUT - update shared txn', () => {
+    test('it returns a 400 with an invalid input', async () => {
+      const { req, res } = mockReqRes('PUT');
+      sessionMock.mockResolvedValueOnce({ user: { email: 'test-user' } });
+      const updateSharedTxnInput = {
+        invalid: 'input',
+      };
+      req._setBody(updateSharedTxnInput);
+      await bySharedTxnIdHandler(req, res);
+
+      expect(res.statusCode).toBe(400);
+    });
   });
 });
--- client/pages/api/v1/trackers/[trackerId]/transactions/[transactionId].ts
+++ client/pages/api/v1/trackers/[trackerId]/transactions/[transactionId].ts
@@ -1,5 +1,21 @@
+import { SubcategoryKeys } from 'data/categories';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from 'next-auth';
+import { withTryCatch } from 'utils/withTryCatch';
+import { z, ZodError } from 'zod';
+
+const updateSharedTxnPayloadSchema = z.object({
+  id: z.string(),
+  date: z.number(),
+  amount: z.number(),
+  location: z.string(),
+  category: SubcategoryKeys,
+  tracker: z.string(),
+  participants: z.array(z.string()).min(2),
+  payer: z.string(),
+  details: z.string(),
+  unsettled: z.boolean().optional(),
+});
 
 export default async function bySharedTxnIdHandler(
   req: NextApiRequest,
@@ -12,5 +28,12 @@ export default async function bySharedTxnIdHandler(
   const session = getServerSession();
   if (!session) {
     return res.status(401).json({ error: 'no valid session found' });
+  }
+
+  let [parsed, err] = withTryCatch(() =>
+    updateSharedTxnPayloadSchema.parse(req.body),
+  );
+  if (err instanceof ZodError) {
+    return res.status(400).json({ error: 'invalid input' });
   }
 }

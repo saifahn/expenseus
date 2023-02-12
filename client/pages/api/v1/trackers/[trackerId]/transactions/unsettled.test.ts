f58001d437a8206763495590b19bdda950417016
Sean Li <sean.saifahn.li@gmail.com>
2023-02-12 11:53:57 +0900

    wip: add 401

--- client/pages/api/v1/trackers/[trackerId]/transactions/unsettled.test.ts
+++ client/pages/api/v1/trackers/[trackerId]/transactions/unsettled.test.ts
@@ -8,4 +8,17 @@ describe('getUnsettledTxnsByTrackerHandler', () => {
 
     expect(res.statusCode).toBe(405);
   });
+
+  test('it returns 401 when there is no valid session', async () => {
+    const { req, res } = mockReqRes('GET');
+    await getUnsettledTxnsByTrackerHandler(req, res);
+
+    expect(res.statusCode).toBe(401);
+  });
+
+  test.todo('it returns 404 when there is no tracker');
+  test.todo('it returns 403 if the user is not part of the tracker');
+  test.todo(
+    'it returns the transactions, debtee as the logged in user, debtor as the other user in the tracker, and amount owed as a number',
+  );
 });
--- client/pages/api/v1/trackers/[trackerId]/transactions/unsettled.ts
+++ client/pages/api/v1/trackers/[trackerId]/transactions/unsettled.ts
@@ -1,4 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
 
 export default async function getUnsettledTxnsByTrackerHandler(
   req: NextApiRequest,
@@ -6,5 +7,10 @@ export default async function getUnsettledTxnsByTrackerHandler(
 ) {
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'invalid method' });
+  }
+
+  const session = getServerSession();
+  if (!session) {
+    return res.status(401).json({ error: 'no valid session found' });
   }
 }

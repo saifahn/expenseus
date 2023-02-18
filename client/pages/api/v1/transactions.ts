b320d92a0e16c0a2a4c3e72296117eeb1dcdc160
Sean Li <sean.saifahn.li@gmail.com>
2023-02-18 21:59:19 +0900

    feat: add session checking for creating txns

--- client/pages/api/v1/transactions.ts
+++ client/pages/api/v1/transactions.ts
@@ -4,8 +4,10 @@
 import { SubcategoryKeys } from 'data/categories';
 import { setUpTxnRepo } from 'ddb/setUpRepos';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { getServerSession } from 'next-auth';
 import { withAsyncTryCatch, withTryCatch } from 'utils/withTryCatch';
-import { z, ZodError } from 'zod';
+import { z } from 'zod';
+import { authOptions } from '../auth/[...nextauth]';
 
 const createTxnPayloadSchema = z.object({
   userId: z.string().min(1),
@@ -26,8 +28,20 @@ export default async function createTxnHandler(
     return res.status(405).json({ error: 'invalid method' });
   }
 
-  let [parsed, err] = withTryCatch(() =>
-    createTxnPayloadSchema.parse(JSON.parse(req.body)),
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) {
+    return res.status(401).json({ error: 'no valid session found' });
+  }
+
+  var [jsonParsed, err] = withTryCatch(() => JSON.parse(req.body));
+  if (err) {
+    return res.status(400).json({ error: 'error parsing payload ' });
+  }
+  var [parsed, err] = withTryCatch(() =>
+    createTxnPayloadSchema.parse({
+      userId: session.user?.email,
+      ...jsonParsed,
+    }),
   );
   if (err) {
     return res.status(400).json({ error: 'invalid payload' });

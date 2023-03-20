f0a64f5ff2700c006f31ae8bd59a3026644993e9
Sean Li <sean.saifahn.li@gmail.com>
2023-03-20 13:23:05 +0900

    refactor: remove go code (#136)

--- server/internal/app/errors.go
+++
@@ -1,9 +0,0 @@
-package app
-
-import "errors"
-
-var (
-	ErrDBItemNotFound = errors.New("store: item not found")
-	ErrUserNotInCtx   = errors.New("user not found in context")
-	ErrTxnIDNotInCtx  = errors.New("transactionID not found in context")
-)

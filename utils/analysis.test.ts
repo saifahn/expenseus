8ee1a70d8fbe62758103ef9acd8456f7779ecadb
Sean Li <sean.saifahn.li@gmail.com>
2023-03-20 14:27:26 +0900

    wip: add another test

--- utils/analysis.test.ts
+++ utils/analysis.test.ts
@@ -31,4 +31,40 @@ describe('personalTotalsByMainCategory', () => {
       expect.objectContaining({ category: 'clothing', total: 5400 }),
     );
   });
+
+  test('it returns the expected results based on both txns and shared txns', () => {
+    const testTxns: (SharedTxn | Transaction)[] = [
+      {
+        id: 'test-txn',
+        date: 123456,
+        amount: 9000,
+        location: 'clothes',
+        tracker: 'test-tracker',
+        category: 'clothing.clothing',
+        participants: ['test-user', 'test-user-2'],
+        details: '',
+        payer: 'test-user',
+        split: {
+          'test-user': 0.6,
+          'test-user-2': 0.4,
+        },
+      },
+      {
+        id: 'test-txn',
+        userId: 'test-user',
+        location: 'burger king',
+        amount: 980,
+        date: 123456,
+        category: 'food.eating-out',
+        details: '',
+      },
+    ];
+    const totals = personalTotalsByMainCategory('test-user', testTxns);
+    expect(totals).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ category: 'clothing', total: 5400 }),
+        expect.objectContaining({ category: 'food', total: 980 }),
+      ]),
+    );
+  });
 });

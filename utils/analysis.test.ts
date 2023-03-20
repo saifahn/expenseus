8d4bb98ed1be0f41e4b10774e9284c26628bb27a
Sean Li <sean.saifahn.li@gmail.com>
2023-03-20 15:30:10 +0900

    feat: add personalTotalsBySubcategory

--- utils/analysis.test.ts
+++ utils/analysis.test.ts
@@ -1,6 +1,9 @@
 import { SharedTxn } from 'pages/shared/trackers/[trackerId]';
 import { Transaction } from 'types/Transaction';
-import { personalTotalsByMainCategory } from './analysis';
+import {
+  personalTotalsByMainCategory,
+  personalTotalsBySubcategory,
+} from './analysis';
 
 describe('personalTotalsByMainCategory', () => {
   test('it returns the an empty object when given no transactions', () => {
@@ -35,7 +38,7 @@ describe('personalTotalsByMainCategory', () => {
   test('it returns the expected results based on both txns and shared txns', () => {
     const testTxns: (SharedTxn | Transaction)[] = [
       {
-        id: 'test-txn',
+        id: 'test-shared-txn',
         date: 123456,
         amount: 9000,
         location: 'clothes',
@@ -58,13 +61,53 @@ describe('personalTotalsByMainCategory', () => {
         category: 'food.eating-out',
         details: '',
       },
+      {
+        id: 'test-clothing-txn',
+        userId: 'test-user',
+        location: 'BEAMS',
+        amount: 5000,
+        date: 234567,
+        category: 'clothing.clothing',
+        details: '',
+      },
     ];
     const totals = personalTotalsByMainCategory('test-user', testTxns);
     expect(totals).toEqual(
       expect.arrayContaining([
-        expect.objectContaining({ category: 'clothing', total: 5400 }),
+        expect.objectContaining({ category: 'clothing', total: 10400 }),
         expect.objectContaining({ category: 'food', total: 980 }),
       ]),
+    );
+  });
+});
+
+describe('personalTotalsBySubcategory', () => {
+  test('it returns the an empty object when given no transactions', () => {
+    const totals = personalTotalsBySubcategory('test-user', []);
+    expect(totals).toEqual([]);
+  });
+
+  test('it returns the expected results for shared txn with split', () => {
+    const testTxns: (SharedTxn | Transaction)[] = [
+      {
+        id: 'test-txn',
+        date: 123456,
+        amount: 9000,
+        location: 'clothes',
+        tracker: 'test-tracker',
+        category: 'clothing.clothing',
+        participants: ['test-user', 'test-user-2'],
+        details: '',
+        payer: 'test-user',
+        split: {
+          'test-user': 0.6,
+          'test-user-2': 0.4,
+        },
+      },
+    ];
+    const totals = personalTotalsBySubcategory('test-user', testTxns);
+    expect(totals).toContainEqual(
+      expect.objectContaining({ category: 'clothing.clothing', total: 5400 }),
     );
   });
 });
--- utils/analysis.ts
+++ utils/analysis.ts
@@ -65,6 +65,7 @@ export function personalTotalsByMainCategory(
   txns: (Transaction | SharedTxn)[],
 ) {
   const totals = {} as Record<MainCategoryKey, number>;
+
   for (const txn of txns) {
     const mainCategory = subcategories[txn.category].mainCategory;
     if (!totals[mainCategory]) totals[mainCategory] = 0;
@@ -83,8 +84,38 @@ export function personalTotalsByMainCategory(
 
     totals[mainCategory] += amount;
   }
+
   return Object.entries(totals).map(([category, total]) => ({
     category,
     total,
   })) as { category: MainCategoryKey; total: number }[];
+}
+
+export function personalTotalsBySubcategory(
+  user: string,
+  txns: (Transaction | SharedTxn)[],
+) {
+  const totals = {} as Record<SubcategoryKey, number>;
+
+  for (const txn of txns) {
+    if (!totals[txn.category]) totals[txn.category] = 0;
+    let amount;
+    // if sharedTxn
+    if ('split' in txn) {
+      const split = txn.split?.[user];
+      amount = split
+        ? txn.amount * split
+        : txn.amount * (1 / txn.participants.length);
+    } else {
+      // if regular txn
+      amount = txn.amount;
+    }
+
+    totals[txn.category] += amount;
+  }
+
+  return Object.entries(totals).map(([category, total]) => ({
+    category,
+    total,
+  })) as { category: SubcategoryKey; total: number }[];
 }
